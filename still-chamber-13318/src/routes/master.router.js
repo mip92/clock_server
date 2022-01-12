481284e6b91dbeea992fd5b6b4db408caf8514e3
mip92 <19mip92@gmail.com>
2022-01-12 19:18:32 +0200

    make it better

---
+++ still-chamber-13318/src/routes/master.router.js
@@ -0,0 +1,33 @@
+const Router = require('express').Router;
+const router = new Router();
+const masterController = require('../controller/master.controller')
+const checkRole = require('../middlwares/checkRoleMiddleware')
+const checkRules = require('../middlwares/checkRulesMiddleware')
+let {body} = require('express-validator');
+
+validationCreateMasterBodyRules = [
+    body('name', "name must be longer than 3 symbols").isLength({min: 3}).not().isEmpty().escape(),
+    body('email', 'email must be a valid email format').not().isEmpty().isEmail().normalizeEmail(),
+    body('city_id', 'city_id is required').not().isEmpty().escape()
+];
+validationGetFreeMastersBodyRules = [
+    body('cityId', 'city_id is required').not().isEmpty().escape(),
+    body('dateTime', 'dateTime is required').not().isEmpty(),
+    body('clockSize', 'clockSize is required').not().isEmpty(),
+];
+validationUpdateMasterBodyRules = [
+    body('name', "name must be longer than 3 symbols").isLength({min: 3}).not().isEmpty().escape(),
+    body('email', 'email must be a valid email format').not().isEmpty().isEmail().normalizeEmail(),
+    body('city_id', 'city_id is required').not().isEmpty().escape()
+];
+
+router.post('/', checkRole("ADMIN"), validationCreateMasterBodyRules, checkRules, masterController.createMaster);
+router.get('/', masterController.getAllMasters);
+router.get('/:masterId', masterController.getOneMaster);
+router.put('/', validationCreateMasterBodyRules, checkRules, masterController.updateMaster);
+router.delete('/:masterId', checkRole("ADMIN"), masterController.deleteMaster);
+/*router.post('/timeReservation'/!*,checkRole("ADMIN")*!/,masterController.timeReservation);*/
+router.post('/getFreeMasters', validationGetFreeMastersBodyRules, checkRules, masterController.getFreeMasters);
+
+
+module.exports = router
---
+++ still-chamber-13318/src/services/Order.js
@@ -0,0 +1,16 @@
+class OneOrder{
+    constructor(dateTime, order, user, master,city) {
+        this.dateTime=dateTime
+        this.orderId=order.id;
+        this.clockSize=order.clockSize;
+        this.userId=user.id;
+        this.userEmail=user.email;
+        this.userName=user.name;
+        this.masterId=master.id;
+        this.masterEmail=master.email;
+        this.masterName=master.name;
+        this.cityId=city.id;
+        this.cityName=city.city_name
+    }
+}
+module.exports = OneOrder;
