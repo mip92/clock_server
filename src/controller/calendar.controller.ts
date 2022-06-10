19feaa92e3b0e6cc23f04cdd8531eb3ce3cbf216
unknown <19mip92@gmail.com>
2022-06-10 12:08:18 +0300

    fix bugs

--- src/controller/calendar.controller.ts
+++ src/controller/calendar.controller.ts
@@ -6,17 +6,95 @@ import {OrderModel} from "../models/order.model";
 import {Op} from "sequelize";
 
 
-type masterIdParam = {
+type getMonthParam = {
     masterId?: string
     correctData?: string
 }
 
+type getWeekParam = {
+    masterId?: string
+    correctWeek?: string
+}
+
 class CalendarController {
-    async getMonth(req: CustomRequest<null, null, masterIdParam, null>, res: Response, next: NextFunction) {
+    async getMonth(req: CustomRequest<null, null, getMonthParam, null>, res: Response, next: NextFunction) {
         try {
             const {masterId, correctData} = req.query
             if (!masterId || !correctData) return next(ApiError.BadRequest("master not found"))
             const date: Date = new Date(correctData)
+            date.setHours(0)
+            date.setMinutes(0)
+            date.setSeconds(0)
+            date.setMilliseconds(0)
+            const month = date.getMonth()
+            const firstDay = date.setDate(1)
+            const dayOfWeek = date.getDay()
+            const nextMontFistDay = new Date(firstDay).setMonth(month + 1)
+            const DaysOfMonth: number = Math.round((+new Date(nextMontFistDay) - +new Date(firstDay)) / 1000 / 3600 / 24);
+
+            const correctMonth = Array.from({length: DaysOfMonth + dayOfWeek}, (v, k) => {
+                if (k < dayOfWeek) {
+                    return 0
+                } else return k++ - dayOfWeek + 1
+            });
+            const getOrdersByDay = (day: number): Promise<OrderModel[] | null> => {
+                let startDayOfMonth: Date | null = date
+                if (day === 0) startDayOfMonth = null
+                startDayOfMonth?.setDate(day)
+                let endDayOfMonth: Date | null
+                endDayOfMonth = startDayOfMonth && new Date(date)
+                endDayOfMonth && startDayOfMonth && endDayOfMonth.setDate(startDayOfMonth.getDate() + 1)
+                return new Promise((resolve, reject) => {
+                        if (endDayOfMonth === null) resolve(null)
+                        else
+                            Order.findAll({
+                                attributes: {exclude: ['cityId', 'createdAt', 'dealPrice', 'masterId', 'originalCityName', 'totalPrice', 'updatedAt', 'masterBusyDateId', 'userId']},
+                                include: [{
+                                    model: MasterBusyDate,
+                                    attributes: {exclude: ['createdAt', 'id', 'masterId', 'updatedAt']},
+                                    where: {
+                                        masterId,
+                                        dateTime: {[Op.between]: [startDayOfMonth?.toISOString(), endDayOfMonth.toISOString()]}
+                                    }
+                                }, {
+                                    model: User,
+                                    attributes: {exclude: ['createdAt', 'id', 'updatedAt', 'password', 'role', 'isActivated', 'email', 'activationLink']},
+                                }]
+                            }).then((orders) => {
+                                resolve(orders)
+                            })
+                    }
+                )
+            }
+            Promise.all(correctMonth.map(day => getOrdersByDay(day))).then((results) => {
+                let month: { orders: OrderModel[] | null, date: Date | null, id: number }[] = []
+                results.map((oneDay, key) => {
+                    const myDate = oneDay === null ? null :date.setDate(correctMonth[key])
+                    month.push({orders: oneDay, date: myDate ? new Date(myDate): null, id: key+1})
+                })
+                res.json(month)
+            })
+        } catch (e) {
+            next(ApiError.Internal(`server error`))
+        }
+    }
+    async getWeek(req: CustomRequest<null, null, getWeekParam, null>, res: Response, next: NextFunction) {
+        try {
+            const {masterId, correctWeek} = req.query
+            console.log(masterId, correctWeek)
+            if (!masterId || !correctWeek) return next(ApiError.BadRequest("master not found"))
+
+            let firstDayOfCurYear = new Date();
+            firstDayOfCurYear.setFullYear(firstDayOfCurYear.getFullYear(), 0, 1);
+            console.log(firstDayOfCurYear)
+            console.log(firstDayOfCurYear.getDate()+((+correctWeek)*7))
+            const monday = new Date(firstDayOfCurYear.setDate(firstDayOfCurYear.getDate()+((+correctWeek)*6+4)))
+            monday.setHours(0)
+            monday.setMinutes(0)
+            monday.setSeconds(0)
+            monday.setMilliseconds(0)
+            console.log(monday.toLocaleDateString())
+            const date: Date = new Date(correctWeek)
             date.setHours(0)
             date.setMinutes(0)
             date.setSeconds(0)
--- src/routes/calendar.router.ts
+++ src/routes/calendar.router.ts
@@ -6,6 +6,9 @@ import calendarController from '../controller/calendar.controller';
 router.get('/month', (res: any, req: any, next: any) => {
     calendarController.getMonth(res, req, next)
 });
+router.get('/week', (res: any, req: any, next: any) => {
+    calendarController.getWeek(res, req, next)
+});
 
 
 export default router
