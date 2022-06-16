5512720111b98697e5fbaf243528c3c331cd2c97
mip92 <75443445+mip92@users.noreply.github.com>
2022-06-16 12:49:41 +0300

    Update checkRulesMiddleware.ts

--- src/middlwares/checkRulesMiddleware.ts
+++ src/middlwares/checkRulesMiddleware.ts
@@ -4,7 +4,6 @@ import {validationResult} from "express-validator";
 export default function (req: express.Request, res: express.Response, next: express.NextFunction) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.log(errors)
         return res.status(422).json({errors: errors.array()});
     }
     next();
